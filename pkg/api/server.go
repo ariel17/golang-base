c58162d565e3ee6181feb7fb2727f351df269b34
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2022-04-08 15:28:44 -0300

    Adds Swagger UI interface.

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
 
 	"github.com/ariel17/golang-base/pkg/configs"
 )
@@ -12,9 +14,27 @@ const statusPath = "/status"
 
 // StartServer creates a new instance of HTTP server with indicated handlers
 // configured and begins serving content.
+//
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth
 func StartServer() {
 	r := gin.Default()
 	r.GET(statusPath, statusHandler)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	if err := r.Run(fmt.Sprintf(":%d", configs.GetPort())); err != nil {
 		panic(err)
